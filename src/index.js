fb32aca44d1f85f854656d0f623abb81f6ee0dc0
Sergio Majé <smajefranco@gmail.com>
2021-06-13 19:10:48 -0500

    Server structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Settings
+
+// Middleware, son funciones que se ejecuten antes de que lleguen a las rutas
+
+// Routes
+
+// Static files
+
+// Starting the server
+app.listen(3000, () => {
+    console.log('server on port 3000');
+});
